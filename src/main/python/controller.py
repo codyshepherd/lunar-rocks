cbd3e4a044d7baa15adf0e008d17c4134b921103
cody <cls9@pdx.edu>
2017-11-14 13:48:23 -0800

    Added handler for msgID 112; started msgID 100

--- src/main/python/controller.py
+++ src/main/python/controller.py
@@ -1,20 +1,64 @@
 import numpy as np
 import random
 import uuid
+import logging
+import os
 
 DEFAULT_TONES = 13
 DEFAULT_BEATS = 8
 MAX_CLIENTS = 1000
 DEFAULT_TEMPO = 8
+LOG_NAME = "controller.log"
+os.remove(LOG_NAME)
+
+LOGGER = logging.getLogger(LOG_NAME)
+logging.basicConfig(filename=LOG_NAME,level=logging.DEBUG)
 
 class Track:
 
     def __init__(self, trackID, dimensions=(DEFAULT_TONES, DEFAULT_BEATS), tempo=DEFAULT_TEMPO):
         self.trackID = trackID              # String
         self.clientID = ""                  # UUID
-        self.grid = np.zeros(dimensions)
+        self.grid = np.zeros(dimensions, dtype=int).tolist() # 2D list of ints
+        self.dimensions = dimensions        # tuple of ints
+
+    def update(self, trk):
+        """
+        update self from trk dict
+
+        :param trk: dict in same format as output of export()
+        """
+        LOGGER.debug("update() started")
+        newgrid = trk.get('grid')
+        if not newgrid:
+            LOGGER.error("No new grid state provided to Track.update()")
+            return False
+
+        if not self.check_dimensions(newgrid):
+            LOGGER.error("New grid has wrong dimensions")
+            return False
+
+        self.grid = newgrid 
+        return True
+
+    def check_dimensions(self, grd):
+        """
+        Ensures dimensions of given state matches those of self.dimensions
+
+        :param grd: a 2-D list
+        """
+        rows = len(grd)
+        cols = len(grd[0])
+
+        if not (rows==self.dimensions[0] and cols==self.dimensions[1]):
+            return False
+
+        return True
 
     def export(self):
+        """
+        exports internal parametrs as json-serializable dict
+        """
         return {
             "trackID": self.trackID,
             "clientID": self.clientID,
@@ -27,6 +71,25 @@ class Session:
         self.clientlist = []
         self.sessionID = sessionID
         self.tracks = [Track(str(sessionID) + 'a'), Track(str(sessionID) + 'b')]
+
+    def update(self, sess):
+        """
+        update self from sess dict
+
+        :param sess: a dict in same format as output of Session.export()
+        """
+        LOGGER.debug("update() started")
+        trackslist = sess.get('tracks')
+        if not trackslist:
+            LOGGER.error("No tracklist provided by sess argument")
+            return False
+
+        for (new, trackobj) in zip(trackslist, self.tracks):
+            if not trackobj.update(new):
+                return False
+
+        return True
+            
 
     def add_client(self, id):
         if id in self.clientlist:
@@ -68,7 +131,7 @@ class Controller:
 
     def __init__(self):
         self.clients = {} #(UUID: String)
-        self.client_sessions = {} #(UUID: List(Session))
+        self.client_sessions = {} #(UUID: List(SessionID))
         self.sessions = {} #(SessionID: Session)
 
     def get_session(self, sid):
@@ -137,7 +200,7 @@ class Controller:
             return False
 
         sess.add_client(cid)
-        self.client_sessions[cid].append(sess)
+        self.client_sessions[cid].append(sess.sessionID)
         return True
 
     def client_leave(self, cid, sid):
@@ -155,12 +218,43 @@ class Controller:
             return False
 
         c_sessions = self.client_sessions[cid]
-        self.client_sessions[cid] = filter(lambda x: x.sessionID != sid, c_sessions)
+        #self.client_sessions[cid] = filter(lambda x: x != sid, c_sessions)
+        self.client_sessions[cid] = [x for x in c_sessions if x != sid]
 
         if sess.is_empty():
             del self.sessions[sid]
 
+        return True
 
+    def update_session(self, cid, sess):
+        """
+        POST-style update from client
+
+        :param cid: string - clientID
+        :param sess: dict - session represented as dict, same as output of Session.export()
+        """
+        LOGGER.debug("update_session() started")
+        sid = sess.get('sessionID')
+
+        if not sid:
+            LOGGER.error("No session ID provided in argument!")
+            return False
+
+        sessionIDs = self.client_sessions.get(cid)
+        if not sessionIDs or sid not in sessionIDs:
+            LOGGER.error("Wanted to update a session it didn't own.")
+            return False
+
+        session = self.sessions.get(sid)
+        if not session:
+            LOGGER.error("Session not found")
+            return False
+
+        if not session.update(sess):
+            return False
+        else:
+            return True
+        
 
 
 
