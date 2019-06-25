0728ea2d861abcc350518af9311d40a5f327de7e
Cody Shepherd <cody.shepherd@canonical.com>
2019-06-25 14:31:49 -0700

    Add token struct

---
+++ go/src/accounts/token.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"time"
+)
+
+type TokenType int
+
+// Token types map to natural numbers starting with 0
+const (
+	Bigfoot TokenType = iota
+	Pixie
+)
+
+type Token struct {
+	TokenString string
+	Type        TokenType
+	Valid       bool
+	Expires     time.Time
+}
