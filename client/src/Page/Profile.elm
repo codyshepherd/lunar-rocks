f514f639edff6b9ed83b5d3639e14498726dfa05
Brian Ginsburg <brian@brianginsburg.com>
2019-08-03 21:52:15 -0700

    Add profile GET request
    Add a GET request that calls to the accounts service to request
    profile details.

--- client/src/Page/Profile.elm
+++ client/src/Page/Profile.elm
@@ -1,19 +1,38 @@
 module Page.Profile exposing (Model, Msg(..), init, update, view)
 
+import Api
 import Element exposing (..)
 import Element.Events exposing (..)
 import Element.Font as Font
 import Element.Input as Input
 import Fonts
+import Http
+import Json.Decode as Decode exposing (Decoder, field, string)
+import Json.Decode.Pipeline exposing (required)
+import Session exposing (Session)
 
 
 type alias Model =
-    Int
+    { session : Session
+    , profile : Maybe Profile
+    }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( 0, Cmd.none )
+type alias Profile =
+    { email : String }
+
+
+init : Session -> String -> ( Model, Cmd Msg )
+init session username =
+    let
+        maybeCred =
+            Session.cred session
+    in
+    ( { session = session
+      , profile = Nothing
+      }
+    , fetchProfile session username
+    )
 
 
 
@@ -21,18 +40,25 @@ init =
 
 
 type Msg
-    = Increment
-    | Decrement
+    = CompletedProfileLoad (Result Http.Error Profile)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            ( model + 1, Cmd.none )
+        CompletedProfileLoad (Err error) ->
+            let
+                debug =
+                    Debug.log "Error requesting profile: " error
+            in
+            ( model, Cmd.none )
 
-        Decrement ->
-            ( model - 1, Cmd.none )
+        CompletedProfileLoad (Ok profile) ->
+            let
+                debug =
+                    Debug.log "Profile returned from server: " profile
+            in
+            ( { model | profile = Just profile }, Cmd.none )
 
 
 
@@ -42,19 +68,50 @@ update msg model =
 view : Model -> Element Msg
 view model =
     row [ centerX, width fill, paddingXY 0 10 ]
-        [ column [ centerX, spacing 10, Font.family Fonts.quattrocentoFont ]
-            [ text "Viewing the profile page"
-            , row [ width fill ]
-                [ column [ centerX ]
-                    [ Input.button [ centerX ] { onPress = Just Increment, label = text "+" }
-                    , el [ centerX ] <| text (String.fromInt model)
-                    , Input.button [ centerX ] { onPress = Just Decrement, label = text "-" }
+        [ column [ centerX, spacing 10, Font.family Fonts.quattrocentoFont ] <|
+            case model.profile of
+                Just profile ->
+                    [ row [ width fill ]
+                        [ column [ centerX ]
+                            [ el [ centerX ] <|
+                                text "Your email:"
+                            , el
+                                [ centerX ]
+                              <|
+                                text profile.email
+                            ]
+                        ]
                     ]
-                ]
-            ]
+
+                Nothing ->
+                    []
         ]
+
+
+
+-- SUBSCRIPTIONS
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
+
+
+
+-- HTTP
+
+
+fetchProfile : Session -> String -> Cmd Msg
+fetchProfile session username =
+    let
+        maybeCred =
+            Session.cred session
+    in
+    Api.get (Api.toUrl [ username ] []) maybeCred profileDecoder
+        |> Http.send CompletedProfileLoad
+
+
+profileDecoder : Decoder Profile
+profileDecoder =
+    Decode.succeed Profile
+        |> required "email" Decode.string
