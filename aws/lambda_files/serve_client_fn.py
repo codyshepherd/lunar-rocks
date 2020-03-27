809cb6ac07f2909575892f2c8b91be04f9bde926
Cody Shepherd <cody.shepherd@canonical.com>
2020-03-26 19:17:20 -0700

    Web page server almost running; some problem with domain name mapping
    that may simply be slowness of custom NS records to populate in google
    domains

--- aws/lambda_files/serve_client_fn.py
+++ aws/lambda_files/serve_client_fn.py
@@ -1,3 +1,35 @@
+import boto3
+import json
+import os
+
+s3 = boto3.client('s3')
 
 def handler(event, context):
     print("serve_client_fn")
+    method = event['httpMethod']
+    page_bucket_name = os.environ['S3_BUCKET']
+
+    path = event.get('path', '')
+    widget_name = path.split('/')[1] if path.startswith('/') else path;
+
+    if method == "GET":
+      # GET / to get the index.html page
+      if path == "/":
+        # We grab the current file pointer first
+        # TODO: using a current file may not be necessary with versioned s3 bucket
+        current = s3.get_object(Bucket=page_bucket_name, Key='current.txt')
+        index_target = current['Body'].read().decode('utf-8').strip()
+        print(f'index target: {index_target}')
+
+        index_page = s3.get_object(Bucket=page_bucket_name, Key=index_target)
+
+        return {
+          "body": json.dumps(index_page['Body'].read().decode('utf-8')),
+          "headers": {
+            'content-type': "text/html",
+          },
+          "isBase64Encoded": False,
+          "statusCode": 200,
+        }
+
+
